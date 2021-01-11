4f9112c63021bee78db0ff92347b37aa04fb756b
Uday Garg <u.garg14@gmail.com>
2021-01-11 12:59:29 +0530

    implemented basic weather app

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,25 +1,26 @@
 package com.example.weatherapp;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,59 +28,63 @@ public class MainActivity extends AppCompatActivity {
     TextView textView1;
     EditText editText;
     Button button;
-    String main="";
-    String description="";
+    String main = "";
+    String description = "";
 
-    public void checkWeather(View view) throws ExecutionException, InterruptedException {
-
-
+    public void checkWeather(View view) {
 
 
         GetWeatherJSON json = new GetWeatherJSON();
-        json.execute("https://api.openweathermap.org/data/2.5/weather?q="+editText.getText()+"&appid=YOURAPIKEY&units=metric").get();
+        try {
+            String encodedString = URLEncoder.encode(editText.getText().toString(), "UTF-8");
+            json.execute("https://api.openweathermap.org/data/2.5/weather?q=" + encodedString + "&appid=a9f0de43f7ae80b8312ceb33a386b6b3&units=metric").get();
 
-
-
-
-
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Could not find weather >.<", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView1=findViewById(R.id.textView2);
-        editText=findViewById(R.id.editTextTextPersonName);
+        textView1 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editTextTextPersonName);
+        ImageView imageView = findViewById(R.id.imageView);
+        textView1.setVisibility(View.VISIBLE);
 
 
     }
-    class GetWeatherJSON extends AsyncTask<String,Void,String>{
+
+    class GetWeatherJSON extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
-            String result="";
+            String result = "";
 
             try {
-                URL url=new URL(urls[0]);
+                URL url = new URL(urls[0]);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 InputStream inputStream = connection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 int data = inputStreamReader.read();
 
-                while(data!=-1){
-                    char current  = (char) data;
-                    result+=current;
-                    data=inputStreamReader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
                 }
                 return result;
-            } catch (MalformedURLException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-                return null;
-            } catch (IOException e) {
-                e.printStackTrace();
+
                 return null;
             }
         }
+
 
         @Override
         protected void onPostExecute(String s) {
@@ -87,25 +92,28 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 JSONObject json = new JSONObject(s);
-                if(json!=null) {
-                    String weatherInfo = json.getString("weather");
-                    JSONArray weatherArray = new JSONArray(weatherInfo);
 
-                    for (int i = 0; i < weatherArray.length(); i++) {
-                        JSONObject jsonObject = weatherArray.getJSONObject(i);
+                String weatherInfo = json.getString("weather");
+                JSONArray weatherArray = new JSONArray(weatherInfo);
 
-                        main = jsonObject.getString("main");
-                        description = jsonObject.getString("description");
-                        System.out.println(main + "" + description);
-                    }
+                for (int i = 0; i < weatherArray.length(); i++) {
+                    JSONObject jsonObject = weatherArray.getJSONObject(i);
 
+                    main = jsonObject.getString("main");
+                    description = jsonObject.getString("description");
+                    System.out.println(main + "" + description);
+                }
+                if (main.isEmpty() && description.isEmpty()) {
+                    textView1.setText("Please enter a valid city");
+                } else {
                     textView1.setText(main + " : " + description);
                 }
-                else{
-                    textView1.setText("Please select a valid city");
-                }
+
+
             } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Could not find weather >.<", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
+
             }
         }
     }
